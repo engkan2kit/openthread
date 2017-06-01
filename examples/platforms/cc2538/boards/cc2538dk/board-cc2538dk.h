f9d3c9db35725cb124193d118d516ebdad4a201c
Stuart Longland <stuartl@vrt.com.au>
2017-06-01 15:10:25 +1000

    WH-289: cc2538: Move CC2538DK functions to .c file.
    Rather than inlining them, since the code style doesn't appear to
    support that.

--- examples/platforms/cc2538/boards/cc2538dk/board-cc2538dk.h
+++ examples/platforms/cc2538/boards/cc2538dk/board-cc2538dk.h
@@ -46,86 +46,53 @@
 /* ----- LEDs ----- */
 
 /* LED port and pin definitions */
-#define CC2538DK_LED1_PORT      (2)     /**< LED1 port number */
-#define CC2538DK_LED1_PIN       (0)     /**< LED1 pin number */
-#define CC2538DK_LED2_PORT      (2)     /**< LED2 port number */
-#define CC2538DK_LED2_PIN       (1)     /**< LED2 pin number */
-#define CC2538DK_LED3_PORT      (2)     /**< LED3 port number */
-#define CC2538DK_LED3_PIN       (2)     /**< LED3 pin number */
-#define CC2538DK_LED4_PORT      (2)     /**< LED4 port number */
-#define CC2538DK_LED4_PIN       (3)     /**< LED4 pin number */
+#define CC2538DK_LED1_PORT          (2) /**< LED1 port number */
+#define CC2538DK_LED1_PIN           (0) /**< LED1 pin number */
+#define CC2538DK_LED2_PORT          (2) /**< LED2 port number */
+#define CC2538DK_LED2_PIN           (1) /**< LED2 pin number */
+#define CC2538DK_LED3_PORT          (2) /**< LED3 port number */
+#define CC2538DK_LED3_PIN           (2) /**< LED3 pin number */
+#define CC2538DK_LED4_PORT          (2) /**< LED4 port number */
+#define CC2538DK_LED4_PIN           (3) /**< LED4 pin number */
 
 /**
  * Initialise the LED GPIOs as outputs.
  */
-static inline void cc2538dkLedInit() {
-    /* Place under software control */
-    cc2538GpioSoftwareControl(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
-    /* Make the pins all outputs */
-    cc2538GpioDirOutput(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
-    cc2538GpioDirOutput(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
-    cc2538GpioDirOutput(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
-    cc2538GpioDirOutput(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
-}
+void cc2538dkLedInit();
 
 /* ----- Buttons ----- */
 
-#define CC2538_BTN_LEFT_PORT    (2)     /**< LEFT pushbutton port number */
-#define CC2538_BTN_LEFT_PIN     (4)     /**< LEFT pushbutton pin number */
-#define CC2538_BTN_RIGHT_PORT   (2)     /**< RIGHT pushbutton port number */
-#define CC2538_BTN_RIGHT_PIN    (5)     /**< RIGHT pushbutton pin number */
-#define CC2538_BTN_UP_PORT      (2)     /**< UP pushbutton port number */
-#define CC2538_BTN_UP_PIN       (6)     /**< UP pushbutton pin number */
-#define CC2538_BTN_DOWN_PORT    (2)     /**< DOWN pushbutton port number */
-#define CC2538_BTN_DOWN_PIN     (7)     /**< DOWN pushbutton pin number */
-#define CC2538_BTN_SELECT_PORT  (0)     /**< SELECT pushbutton port number */
-#define CC2538_BTN_SELECT_PIN   (3)     /**< SELECT pushbutton pin number */
+#define CC2538DK_BTN_LEFT_PORT      (2) /**< LEFT pushbutton port number */
+#define CC2538DK_BTN_LEFT_PIN       (4) /**< LEFT pushbutton pin number */
+#define CC2538DK_BTN_RIGHT_PORT     (2) /**< RIGHT pushbutton port number */
+#define CC2538DK_BTN_RIGHT_PIN      (5) /**< RIGHT pushbutton pin number */
+#define CC2538DK_BTN_UP_PORT        (2) /**< UP pushbutton port number */
+#define CC2538DK_BTN_UP_PIN         (6) /**< UP pushbutton pin number */
+#define CC2538DK_BTN_DOWN_PORT      (2) /**< DOWN pushbutton port number */
+#define CC2538DK_BTN_DOWN_PIN       (7) /**< DOWN pushbutton pin number */
+#define CC2538DK_BTN_SELECT_PORT    (0) /**< SELECT pushbutton port number */
+#define CC2538DK_BTN_SELECT_PIN     (3) /**< SELECT pushbutton pin number */
 
 /**
  * Initialise the pushbutton GPIOs as outputs.
  */
-static inline void cc2538dkButtonInit() {
-    /* Place under software control */
-    cc2538GpioSoftwareControl(CC2538DK_BTN_LEFT_PORT, CC2538DK_BTN_LEFT_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_BTN_RIGHT_PORT, CC2538DK_BTN_RIGHT_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_BTN_UP_PORT, CC2538DK_BTN_UP_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_BTN_DOWN_PORT, CC2538DK_BTN_DOWN_PIN);
-    cc2538GpioSoftwareControl(CC2538DK_BTN_SELECT_PORT, CC2538DK_BTN_SELECT_PIN);
-    /* Make the pins all inputs */
-    cc2538GpioDirInput(CC2538DK_BTN_LEFT_PORT, CC2538DK_BTN_LEFT_PIN);
-    cc2538GpioDirInput(CC2538DK_BTN_RIGHT_PORT, CC2538DK_BTN_RIGHT_PIN);
-    cc2538GpioDirInput(CC2538DK_BTN_UP_PORT, CC2538DK_BTN_UP_PIN);
-    cc2538GpioDirInput(CC2538DK_BTN_DOWN_PORT, CC2538DK_BTN_DOWN_PIN);
-    cc2538GpioDirInput(CC2538DK_BTN_SELECT_PORT, CC2538DK_BTN_SELECT_PIN);
-}
+void cc2538dkButtonInit();
 
 /* ----- Light Sensor (Analogue) ----- */
 
-#define CC2538DK_ALS_POWER_PORT (0)     /**< ALS power enable port number */
-#define CC2538DK_ALS_POWER_PIN  (7)     /**< ALS power enable pin number */
-#define CC2538DK_ALS_SIG_PORT   (0)     /**< ALS signal port number */
-#define CC2538DK_ALS_SIG_PIN    (6)     /**< ALS signal pin number */
+#define CC2538DK_ALS_POWER_PORT     (0) /**< ALS power enable port number */
+#define CC2538DK_ALS_POWER_PIN      (7) /**< ALS power enable pin number */
+#define CC2538DK_ALS_SIG_PORT       (0) /**< ALS signal port number */
+#define CC2538DK_ALS_SIG_PIN        (6) /**< ALS signal pin number */
 
 /**
  * Initialise the ambient light sensor.
  */
-static inline void cc2538dkAlsInit() {
-    /* Make the signal pin an analogue input */
-    cc2538AdcPinInit(CC2538DK_ALS_SIG_PORT, CC2538DK_ALS_SIG_PIN);
-    /* Make the power enable pin an output and turn it on */
-    cc2538GpioSoftwareControl(CC2538DK_ALS_POWER_PORT, CC2538DK_ALS_POWER_PIN);
-    cc2538GpioDirOutput(CC2538DK_ALS_POWER_PORT, CC2538DK_ALS_POWER_PIN);
-    cc2538GpioSetPin(CC2538DK_ALS_POWER_PORT, CC2538DK_ALS_POWER_PIN);
-}
+void cc2538dkAlsInit();
 
 /**
  * Read the ambient light level.
  */
-static inline int16_t cc2538dkAlsRead() {
-    return cc2538AdcReadChannel(CC2538DK_ALS_SIG_PIN);
-}
+int16_t cc2538dkAlsRead();
 
 #endif  // BOARD_CC2538DK_H_
--- examples/platforms/cc2538/boards/cc2538dk/board.c
+++ examples/platforms/cc2538/boards/cc2538dk/board.c
@@ -36,8 +36,56 @@
 
 otInstance *sInstance;
 
+void cc2538dkLedInit()
+{
+    /* Place under software control */
+    cc2538GpioSoftwareControl(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+    /* Make the pins all outputs */
+    cc2538GpioDirOutput(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+    cc2538GpioDirOutput(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+    cc2538GpioDirOutput(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+    cc2538GpioDirOutput(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+}
+
+void cc2538dkButtonInit()
+{
+    /* Place under software control */
+    cc2538GpioSoftwareControl(CC2538DK_BTN_LEFT_PORT, CC2538DK_BTN_LEFT_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_BTN_RIGHT_PORT, CC2538DK_BTN_RIGHT_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_BTN_UP_PORT, CC2538DK_BTN_UP_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_BTN_DOWN_PORT, CC2538DK_BTN_DOWN_PIN);
+    cc2538GpioSoftwareControl(CC2538DK_BTN_SELECT_PORT, CC2538DK_BTN_SELECT_PIN);
+    /* Make the pins all inputs */
+    cc2538GpioDirInput(CC2538DK_BTN_LEFT_PORT, CC2538DK_BTN_LEFT_PIN);
+    cc2538GpioDirInput(CC2538DK_BTN_RIGHT_PORT, CC2538DK_BTN_RIGHT_PIN);
+    cc2538GpioDirInput(CC2538DK_BTN_UP_PORT, CC2538DK_BTN_UP_PIN);
+    cc2538GpioDirInput(CC2538DK_BTN_DOWN_PORT, CC2538DK_BTN_DOWN_PIN);
+    cc2538GpioDirInput(CC2538DK_BTN_SELECT_PORT, CC2538DK_BTN_SELECT_PIN);
+}
+
+void cc2538dkAlsInit()
+{
+    /* Make the signal pin an analogue input */
+    cc2538AdcPinInit(CC2538DK_ALS_SIG_PORT, CC2538DK_ALS_SIG_PIN);
+    /* Make the power enable pin an output and turn it on */
+    cc2538GpioSoftwareControl(CC2538DK_ALS_POWER_PORT, CC2538DK_ALS_POWER_PIN);
+    cc2538GpioDirOutput(CC2538DK_ALS_POWER_PORT, CC2538DK_ALS_POWER_PIN);
+    cc2538GpioSetPin(CC2538DK_ALS_POWER_PORT, CC2538DK_ALS_POWER_PIN);
+}
+
+int16_t cc2538dkAlsRead()
+{
+    return cc2538AdcReadChannel(CC2538DK_ALS_SIG_PIN);
+}
+
 void cc2538BoardInit(int argc, char *argv[])
 {
+    (void)argc;
+    (void)argv;
+
     cc2538dkLedInit();
     cc2538dkButtonInit();
     cc2538dkAlsInit();
@@ -45,4 +93,48 @@ void cc2538BoardInit(int argc, char *argv[])
 
 void cc2538BoardProcess(otInstance *aInstance)
 {
+    static uint32_t timer = 0;
+    static uint8_t led = 0;
+    (void)aInstance;
+
+    if (timer)
+    {
+        timer--;
+    }
+    else
+    {
+        timer = 10000;
+        switch (led++) {
+            case 0:
+                cc2538GpioSetPin(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+                cc2538GpioClearPin(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+                cc2538GpioClearPin(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+                cc2538GpioClearPin(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+                break;
+            case 1:
+                cc2538GpioClearPin(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+                cc2538GpioSetPin(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+                cc2538GpioClearPin(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+                cc2538GpioClearPin(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+                break;
+            case 2:
+                cc2538GpioClearPin(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+                cc2538GpioClearPin(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+                cc2538GpioSetPin(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+                cc2538GpioClearPin(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+                break;
+            case 3:
+                cc2538GpioClearPin(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+                cc2538GpioClearPin(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+                cc2538GpioClearPin(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+                cc2538GpioSetPin(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+                break;
+            default:
+                cc2538GpioClearPin(CC2538DK_LED1_PORT, CC2538DK_LED1_PIN);
+                cc2538GpioClearPin(CC2538DK_LED2_PORT, CC2538DK_LED2_PIN);
+                cc2538GpioClearPin(CC2538DK_LED3_PORT, CC2538DK_LED3_PIN);
+                cc2538GpioClearPin(CC2538DK_LED4_PORT, CC2538DK_LED4_PIN);
+                led = 0;
+        }
+    }
 }
